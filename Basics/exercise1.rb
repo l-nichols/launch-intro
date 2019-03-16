a95f23db4c6f614866995983f6dd274bbf61011e
l-nichols <stareyedgirl@gmail.com>
2019-03-16 18:15:57 -0500

    Completed Chapter 1, Exercise 1

---
+++ Basics/exercise1.rb
@@ -0,0 +1,12 @@
+# Add two strings together that, when concatenated, return your first and
+# last name as your full name in one string.
+#
+# "<Firstname> <Lastname>"
+#
+# For example, if your name is John Doe, think about how you can put
+# "John" and "Doe" together to get "John Doe".
+
+first_name = "Elliot"
+last_name = "Nichols"
+
+puts "#{first_name} #{last_name}"
