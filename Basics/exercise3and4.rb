d2a447e24fbed77b8ac8bc903cb2628d5882ec05
l-nichols <stareyedgirl@gmail.com>
2019-03-16 18:49:44 -0500

    Completed Chapter 1, Exercise 4

--- Basics/exercise3and4.rb
+++ Basics/exercise3and4.rb
@@ -1,3 +1,4 @@
+# Exercise 3
 # Write a program that uses a hash to store a list of movie titles
 # with the year they came out. Then use the puts command to make your
 # program print out the year of each movie to the screen.
@@ -16,5 +17,13 @@ movies = {
   "The Dark Knight": 2008,
   "The Lord of the Rings: The Return of the King": 2003
 }
-
+puts "Output for exercise 3:"
 movies.each { |key, value| puts value }
+
+# Exercise 4
+# Use the dates from the previous example and store them in an array.
+# Then make your program output the same thing as exercise 3.
+
+movie_array = movies.collect { |key, value| value  }
+puts "Output for exercise 4:"
+movie_array.each { |year| puts year  }
