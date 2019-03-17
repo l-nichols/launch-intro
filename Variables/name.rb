6f13146a033c5adf860e46f40ad1c85ba96b264e
l-nichols <stareyedgirl@gmail.com>
2019-03-16 21:09:05 -0500

    Completed Chapter 2, Exercise 3

--- Variables/name.rb
+++ Variables/name.rb
@@ -1,3 +1,4 @@
+# Exercise 1
 # Write a program called name.rb that asks the user to type in their
 # name and then prints out a greeting message with their name included.
 
@@ -5,3 +6,10 @@ puts "What's your name?"
 name = gets.chomp
 
 puts "Hi there, #{name}!"
+
+# Exercise 3
+# Add another section onto name.rb that prints the name of the user 10 times.
+# You must do this without explicitly writing the puts method 10 times in a row.
+# Hint: you can use the times method to do something repeatedly.
+
+10.times {puts name}
